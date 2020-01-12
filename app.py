5da3fcaedab353cea1107edebc9c8d20b9dca467
john-thomson <github@auto.jwdt.co.uk>
2020-01-12 04:14:29 +0000

    Added airtable Products defaults.

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 
+from load_config import Config
+
 app = Flask(__name__)
 
 
 config_defaults = {
-
+    "products_table": "Products",
+    "product_name": "Name",
+    "product_description": "Description",
+    "product_photos": "Photos",
+    "product_status": "Status",
+    "product_price": "Price",
+    "orders_table": "Orders"
 }
 
+c = Config(defaults=config_defaults)
 
 @app.route('/')
 def hello_world():
